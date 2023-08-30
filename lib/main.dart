9647a61a118c6dcc763e0788fe4d6984f533e99a
Aditya Mittal <adityamittal0918@gmail.com>
2023-08-30 19:28:20 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
-import 'package:firebase_auth/screens/email_password_login_screen.dart';
-import 'package:firebase_auth/screens/email_passwrod_signup.dart';
-import 'package:firebase_auth/screens/login_screen.dart';
-import 'package:firebase_auth/screens/phone_screen.dart';
+import 'package:firebase_auth_demo/firebase_options.dart';
+import 'package:firebase_auth_demo/screens/email_password_login_screen.dart';
+import 'package:firebase_auth_demo/screens/email_password_signup.dart';
+import 'package:firebase_auth_demo/screens/login_screen.dart';
+import 'package:firebase_auth_demo/screens/phone_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
